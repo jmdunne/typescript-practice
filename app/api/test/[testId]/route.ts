157c5b973fc0f934b87e7d66eec989606c673905
Josh Dunne <joshua.m.dunne@gmail.com>
2024-07-25 14:15:57 -0700

    02B: Added Hono, Clerk Backend, Route and subroutes

--- app/api/test/[testId]/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse, NextRequest } from "next/server";
-
-export const GET = async (
-  request: NextRequest,
-  { params }: { params: { testId: string } }
-) => {
-  return NextResponse.json({ hello: "World!", testId: params.testId });
-};
